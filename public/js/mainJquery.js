87bbd1cdf07b20e736c895ebeac9e7e890e074aa
SibahleMayekiso <mayekisosibahle@gmail.com>
2022-09-09 20:14:32 +0200

    Initial data creation

---
+++ public/js/mainJquery.js
@@ -0,0 +1,25 @@
+//Data Source lists
+let continentList = [
+    {continent: 'Africa'}, 
+    {continent: 'Asia'},
+    {continent: 'Europe'},
+    {continent: 'North America'},
+    {continent: 'Oceania'},
+    {continent: 'South America'},
+];
+
+let countryList = [
+    {continent: 'Africa', country: 'South Africa'}, 
+    {continent: 'Africa', country: 'Lesotho'}, 
+    {continent: 'Africa', country: 'eSwatini'} 
+];
+
+let cityList = [
+    {continent: 'Africa', country: 'South Africa', city: 'Pretoria'},  
+    {continent: 'Africa', country: 'South Africa', city: 'Johannesburg'}, 
+    {continent: 'Africa', country: 'South Africa', city: 'Cape Town'},
+    {continent: 'Africa', country: 'Lesotho', city: 'Maseru'},
+    {continent: 'Africa', country: 'Lesotho', city: 'Butha-Buthe'},  
+    {continent: 'Africa', country: 'Lesotho', city: 'Thaba-Tseka'}  
+];
+
